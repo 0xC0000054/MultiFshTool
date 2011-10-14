b16317fda7da56fe2780192fe6e09d54056dbf3e
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-10-14 07:18:35 +0000

    Minor code cleanup.

--- BlendBmp.cs
+++ BlendBmp.cs
@@ -7,60 +7,61 @@ using FSHLib;
 
 namespace loaddatfsh
 {
-   public class BlendBitmap
+    public class BlendBitmap
     {
        /// <summary>
        /// Blends the fsh bitmap and alpha images
        /// </summary>
        /// <param name="bmpitem">The bitmap item to blend</param>
        /// <returns>The blended bitmap or null</returns>
-       public Bitmap BlendBmp(BitmapItem bmpitem)
+       public static Bitmap BlendBmp(BitmapItem bmpitem)
        {
-            Bitmap image = null;
             if (bmpitem.Bitmap != null && bmpitem.Alpha != null)
-            {
-                image = new Bitmap(bmpitem.Bitmap.Width, bmpitem.Bitmap.Height, PixelFormat.Format32bppArgb);
-
-                Bitmap bmpalpha = null;
-                Bitmap colorbmp = null;
-               
-                colorbmp = new Bitmap(bmpitem.Bitmap);
-                bmpalpha = new Bitmap(bmpitem.Alpha);
-               
-                BitmapData colordata = colorbmp.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-                BitmapData alphadata = bmpalpha.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-                BitmapData bdata = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
-                IntPtr scan0 = bdata.Scan0;
-                unsafe
+            {            
+                Bitmap image = null;
+                using (Bitmap temp = new Bitmap(bmpitem.Bitmap.Width, bmpitem.Bitmap.Height, PixelFormat.Format32bppArgb))
                 {
-                    byte* clrdata = (byte*)(void*)colordata.Scan0;
-                    byte* aldata = (byte*)(void*)alphadata.Scan0;
-                    byte* destdata = (byte*)(void*)scan0;
-                    int offset = bdata.Stride - image.Width * 4;
-                    int clroffset = colordata.Stride - image.Width * 4;
-                    int aloffset = alphadata.Stride - image.Width * 4;
-                    for (int y = 0; y < image.Height; y++)
-                    {
-                        for (int x = 0; x < image.Width; x++)
-                        {
-                            destdata[3] = aldata[0]; // copy the blue alpha map channel to the blended image alpha channel
-                            destdata[0] = clrdata[0]; // blue 
-                            destdata[1] = clrdata[1]; // green 
-                            destdata[2] = clrdata[2]; // red
+                   
 
-                            destdata += 4;
-                            clrdata += 4;
-                            aldata += 4;
+                    using (Bitmap bmpalpha = new Bitmap(bmpitem.Bitmap))
+                    using (Bitmap colorbmp = new Bitmap(bmpitem.Alpha))
+                    {
+                        BitmapData colordata = colorbmp.LockBits(new Rectangle(0, 0, temp.Width, temp.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                        BitmapData alphadata = bmpalpha.LockBits(new Rectangle(0, 0, temp.Width, temp.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                        BitmapData bdata = temp.LockBits(new Rectangle(0, 0, temp.Width, temp.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+                        unsafe
+                        {
+                            byte* clrdata = (byte*)(void*)colordata.Scan0;
+                            byte* aldata = (byte*)(void*)alphadata.Scan0;
+                            byte* destdata = (byte*)(void*)bdata.Scan0;
+                            int offset = bdata.Stride - temp.Width * 4;
+                            int clroffset = colordata.Stride - temp.Width * 4;
+                            int aloffset = alphadata.Stride - temp.Width * 4;
+                            for (int y = 0; y < temp.Height; y++)
+                            {
+                                for (int x = 0; x < temp.Width; x++)
+                                {
+                                    destdata[3] = aldata[0]; // copy the blue alpha map channel to the blended image alpha channel
+                                    destdata[0] = clrdata[0]; // blue 
+                                    destdata[1] = clrdata[1]; // green 
+                                    destdata[2] = clrdata[2]; // red
+
+                                    destdata += 4;
+                                    clrdata += 4;
+                                    aldata += 4;
+                                }
+
+                            }
+
                         }
-                        destdata += offset;
-                        clrdata += clroffset;
-                        aldata += aloffset;
+                        colorbmp.UnlockBits(colordata);
+                        bmpalpha.UnlockBits(alphadata);                     
+                        temp.UnlockBits(bdata);
                     }
 
+                    image = (Bitmap)temp.Clone();
                 }
-                colorbmp.UnlockBits(colordata);
-                bmpalpha.UnlockBits(alphadata);
-                image.UnlockBits(bdata);
+
                 return image;
             }
             else
