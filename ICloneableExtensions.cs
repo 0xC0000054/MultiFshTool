bab8f24fc0a0d2ef4be2bc2452914b08013ccd42
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-10-14 07:20:16 +0000

    Upgraded to NET 3.5.

---
+++ ICloneableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace loaddatfsh
+{
+    static class ICloneableExtensions
+    {
+        public static T Clone<T>(this ICloneable obj)
+        {
+            return (T)obj.Clone();
+        }
+    }
+}
