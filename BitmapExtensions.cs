6c213bbfeb038f053304bc7d8750c2cd875e4037
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-11-28 06:06:38 +0000

    Added an extension method to clone the bitmap with the specified pixel format.

---
+++ BitmapExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace loaddatfsh
+{
+    static class BitmapExtensions
+    {
+        public static Bitmap Clone(this Bitmap bmp, PixelFormat format)
+        {
+            return bmp.Clone(new Rectangle(0, 0, bmp.Width, bmp.Height), format);
+        }
+    }
+}
