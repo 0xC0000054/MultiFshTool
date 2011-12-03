3579c8233b26d59f5de6c42a5c899cf46d82fbcc
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-12-03 09:18:35 +0000

    The color and alpha bitmap formats are now 24-bit.

--- BlendBmp.cs
+++ BlendBmp.cs
@@ -14,48 +14,52 @@ namespace loaddatfsh
        /// </summary>
        /// <param name="bmpitem">The bitmap item to blend</param>
        /// <returns>The blended bitmap or null</returns>
-       public static Bitmap BlendBmp(FshDatIO.BitmapEntry bmpitem)
+       public static unsafe Bitmap BlendBmp(FshDatIO.BitmapEntry bmpitem)
        {
             if (bmpitem.Bitmap != null && bmpitem.Alpha != null)
             {            
                 Bitmap image = null;
                 using (Bitmap temp = new Bitmap(bmpitem.Bitmap.Width, bmpitem.Bitmap.Height, PixelFormat.Format32bppArgb))
                 {
-                   
+                    int width = temp.Width;
+                    int height = temp.Height;
 
-                    using (Bitmap colorbmp = new Bitmap(bmpitem.Bitmap))
-                    using (Bitmap bmpalpha = new Bitmap(bmpitem.Alpha))
+                    Rectangle lockRect = new Rectangle(0, 0, width, height);
+                    BitmapData colordata = bmpitem.Bitmap.LockBits(lockRect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+                    BitmapData alphadata = bmpitem.Alpha.LockBits(lockRect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+                    BitmapData bdata = temp.LockBits(lockRect, ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+                    try
                     {
-                        BitmapData colordata = colorbmp.LockBits(new Rectangle(0, 0, temp.Width, temp.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-                        BitmapData alphadata = bmpalpha.LockBits(new Rectangle(0, 0, temp.Width, temp.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-                        BitmapData bdata = temp.LockBits(new Rectangle(0, 0, temp.Width, temp.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
-                        unsafe
+                        byte* clrScan0 = (byte*)colordata.Scan0.ToPointer();
+                        byte* alScan0 = (byte*)alphadata.Scan0;
+                        byte* destScan0 = (byte*)bdata.Scan0;
+                        int stride = bdata.Stride;
+                        int clrStride = colordata.Stride;
+                        int alStride = alphadata.Stride;
+                        for (int y = 0; y < height; y++)
                         {
-                            byte* clrdata = (byte*)(void*)colordata.Scan0;
-                            byte* aldata = (byte*)(void*)alphadata.Scan0;
-                            byte* destdata = (byte*)(void*)bdata.Scan0;
-                            int offset = bdata.Stride - temp.Width * 4;
-                            int clroffset = colordata.Stride - temp.Width * 4;
-                            int aloffset = alphadata.Stride - temp.Width * 4;
-                            for (int y = 0; y < temp.Height; y++)
+                            byte* color = clrScan0 + (y * clrStride);
+                            byte* alpha = alScan0 + (y * alStride);
+                            byte* dest = destScan0 + (y * stride);
+                            for (int x = 0; x < width; x++)
                             {
-                                for (int x = 0; x < temp.Width; x++)
-                                {
-                                    destdata[3] = aldata[0]; // copy the blue alpha map channel to the blended image alpha channel
-                                    destdata[0] = clrdata[0]; // blue 
-                                    destdata[1] = clrdata[1]; // green 
-                                    destdata[2] = clrdata[2]; // red
+                                dest[3] = alpha[0]; // copy the blue alpha map channel to the blended image alpha channel
+                                dest[0] = color[0]; // blue 
+                                dest[1] = color[1]; // green 
+                                dest[2] = color[2]; // red
 
-                                    destdata += 4;
-                                    clrdata += 4;
-                                    aldata += 4;
-                                }
-
+                                dest += 4;
+                                color += 3;
+                                alpha += 3;
                             }
 
                         }
-                        colorbmp.UnlockBits(colordata);
-                        bmpalpha.UnlockBits(alphadata);                     
+                    }
+                    finally
+                    {
+                        bmpitem.Bitmap.UnlockBits(colordata);
+                        bmpitem.Alpha.UnlockBits(alphadata);                     
                         temp.UnlockBits(bdata);
                     }
 
