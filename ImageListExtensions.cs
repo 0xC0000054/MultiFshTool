180e0d3144d8647b617b1efbb4e89fbf4fbfa49e
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2012-01-20 21:55:48 +0000

    Initial Commit.

---
+++ ImageListExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Reflection;
+
+namespace loaddatfsh
+{
+    static class ImageListExtensions
+    {
+        private static readonly Size defaultSize = new Size(96, 96); 
+        public static void ScaleListSize(this ImageList list, Bitmap image)
+        {
+            if (list.ImageSize.Width > image.Width || list.ImageSize.Height > image.Height)
+            {
+                int width = Math.Min(list.ImageSize.Width, image.Width);
+                int height = Math.Min(list.ImageSize.Height, image.Height);
+
+                list.ImageSize = new Size(width, height);
+            }
+            else
+            {
+                list.ImageSize = defaultSize;
+            }
+        }
+
+    }
+}
