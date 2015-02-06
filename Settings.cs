cb88f3a4f048d49d1fe1cb76c2330f42a8d3267f
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2015-02-06 00:28:44 +0000

    Removed the unused GetSetting and PutSetting overloads, removed the try block from the constructor.

--- Settings.cs
+++ Settings.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Xml;
+﻿using System.Xml;
 
 namespace loaddatfsh
 {
@@ -12,36 +10,31 @@ namespace loaddatfsh
 
         public Settings(string path)
         {
-            try 
-            {
-                documentPath = path;
-                xmlDocument = new XmlDocument();
-                xmlDocument.Load(documentPath);
-            }
-            catch { xmlDocument.LoadXml("<settings></settings>"); }
-        }
-
-        public int GetSetting(string xPath, int defaultValue)
-        { 
-            return Convert.ToInt16(GetSetting(xPath, defaultValue.ToString(CultureInfo.InvariantCulture)), CultureInfo.InvariantCulture);
-        }
-
-        public void PutSetting(string xPath, int value)
-        { 
-            PutSetting(xPath, value.ToString(CultureInfo.InvariantCulture)); 
+            documentPath = path;
+            xmlDocument = new XmlDocument();
+            xmlDocument.Load(documentPath);
         }
 
         public string GetSetting(string xPath, string defaultValue)
         {
             XmlNode xmlNode = xmlDocument.SelectSingleNode("settings/" + xPath);
-            if (xmlNode != null) { return xmlNode.InnerText; }
-            else { return defaultValue; }
+            if (xmlNode != null) 
+            { 
+                return xmlNode.InnerText; 
+            }
+            else
+            { 
+                return defaultValue;
+            }
         }
 
         public void PutSetting(string xPath, string value)
         {
             XmlNode xmlNode = xmlDocument.SelectSingleNode("settings/" + xPath);
-            if (xmlNode == null) { xmlNode = createMissingNode("settings/" + xPath); }
+            if (xmlNode == null)
+            { 
+                xmlNode = createMissingNode("settings/" + xPath);
+            }
             xmlNode.InnerText = value;
             xmlDocument.Save(documentPath);
         }
@@ -56,7 +49,10 @@ namespace loaddatfsh
             {
                 currentXPath += xPathSection;
                 testNode = xmlDocument.SelectSingleNode(currentXPath);
-                if (testNode == null) { currentNode.InnerXml += "<" + xPathSection + "></" + xPathSection + ">"; }
+                if (testNode == null) 
+                { 
+                    currentNode.InnerXml += "<" + xPathSection + "></" + xPathSection + ">";
+                }
                 currentNode = xmlDocument.SelectSingleNode(currentXPath);
                 currentXPath += "/";
             }
