b8292951abb57cb08cef06231766a6206f4ceedd
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2015-02-23 10:26:32 +0000

    Added an EnableBorderSelect method to prevent the underlying Win32 list view from changing the color of the thumbnails when an item is selected.

--- DelayChangeListView.cs
+++ DelayChangeListView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace loaddatfsh
@@ -19,6 +20,19 @@ namespace loaddatfsh
             this.indexChangedTimer = new Timer();
             this.indexChangedTimer.Interval = 10;
             this.indexChangedTimer.Tick += eventDelayTimer_Tick;
+
+            base.DoubleBuffered = true;
+        }
+
+        /// <summary>
+        /// Enables the border select mode for the native Win32 list view.
+        /// </summary>
+        public void EnableBorderSelect()
+        {
+            // Set the border select extended style to prevent the underlying Win32 list view from changing the color of the thumbnails when an item is selected.
+            NativeEnums.ListViewExtendedStyles style = NativeEnums.ListViewExtendedStyles.BorderSelect;
+
+            NativeMethods.SendMessageW(this.Handle, NativeConstants.LVM_SETEXTENDEDLISTVIEWSTYLE, (UIntPtr)style, (IntPtr)style);
         }
 
         protected override void OnSelectedIndexChanged(EventArgs e)
@@ -42,10 +56,113 @@ namespace loaddatfsh
                     this.indexChangedTimer = null;
                 }
             }
-            
+
             base.Dispose(disposing);
         }
 
+        private static class NativeConstants
+        {
+             internal const uint LVM_FIRST = 0x1000;
+             internal const uint LVM_SETEXTENDEDLISTVIEWSTYLE = LVM_FIRST + 54;
+             internal const uint LVM_GETEXTENDEDLISTVIEWSTYLE = LVM_FIRST + 55;
+        }
 
+        private static class NativeMethods
+        {
+            [DllImport("user32.dll", ExactSpelling = true)]
+            public static extern IntPtr SendMessageW([In()] IntPtr hWnd, [In()] uint Msg, [In()] UIntPtr wParam, [In()] IntPtr lParam);
+        }
+
+        private static class NativeEnums
+        {
+            public enum ListViewExtendedStyles
+            {
+                /// <summary>
+                /// LVS_EX_GRIDLINES
+                /// </summary>
+                GridLines = 0x00000001,
+                /// <summary>
+                /// LVS_EX_SUBITEMIMAGES
+                /// </summary>
+                SubItemImages = 0x00000002,
+                /// <summary>
+                /// LVS_EX_CHECKBOXES
+                /// </summary>
+                CheckBoxes = 0x00000004,
+                /// <summary>
+                /// LVS_EX_TRACKSELECT
+                /// </summary>
+                TrackSelect = 0x00000008,
+                /// <summary>
+                /// LVS_EX_HEADERDRAGDROP
+                /// </summary>
+                HeaderDragDrop = 0x00000010,
+                /// <summary>
+                /// LVS_EX_FULLROWSELECT
+                /// </summary>
+                FullRowSelect = 0x00000020,
+                /// <summary>
+                /// LVS_EX_ONECLICKACTIVATE
+                /// </summary>
+                OneClickActivate = 0x00000040,
+                /// <summary>
+                /// LVS_EX_TWOCLICKACTIVATE
+                /// </summary>
+                TwoClickActivate = 0x00000080,
+                /// <summary>
+                /// LVS_EX_FLATSB
+                /// </summary>
+                FlatsB = 0x00000100,
+                /// <summary>
+                /// LVS_EX_REGIONAL
+                /// </summary>
+                Regional = 0x00000200,
+                /// <summary>
+                /// LVS_EX_INFOTIP
+                /// </summary>
+                InfoTip = 0x00000400,
+                /// <summary>
+                /// LVS_EX_UNDERLINEHOT
+                /// </summary>
+                UnderlineHot = 0x00000800,
+                /// <summary>
+                /// LVS_EX_UNDERLINECOLD
+                /// </summary>
+                UnderlineCold = 0x00001000,
+                /// <summary>
+                /// LVS_EX_MULTIWORKAREAS
+                /// </summary>
+                MultilWorkAreas = 0x00002000,
+                /// <summary>
+                /// LVS_EX_LABELTIP
+                /// </summary>
+                LabelTip = 0x00004000,
+                /// <summary>
+                /// LVS_EX_BORDERSELECT
+                /// </summary>
+                BorderSelect = 0x00008000,
+                /// <summary>
+                /// LVS_EX_DOUBLEBUFFER
+                /// </summary>
+                DoubleBuffer = 0x00010000,
+                /// <summary>
+                /// LVS_EX_HIDELABELS
+                /// </summary>
+                HideLabels = 0x00020000,
+                /// <summary>
+                /// LVS_EX_SINGLEROW
+                /// </summary>
+                SingleRow = 0x00040000,
+                /// <summary>
+                /// LVS_EX_SNAPTOGRID
+                /// </summary>
+                SnapToGrid = 0x00080000,
+                /// <summary>
+                /// LVS_EX_SIMPLESELECT
+                /// </summary>
+                SimpleSelect = 0x00100000
+            }
+
+        }
     }
 }
