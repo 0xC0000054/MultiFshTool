57764f10e8ce9c5a9a44bc81f745ef238720d9ed
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2017-08-27 12:30:29 +0000

    Release version 1.3.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("null45")]
 [assembly: AssemblyProduct("Multi fsh tool")]
-[assembly: AssemblyCopyright("Copyright © 2010-2016 Nicholas Hayes")]
+[assembly: AssemblyCopyright("Copyright © 2010-2017 Nicholas Hayes")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.2.2")]
-[assembly: AssemblyFileVersion("1.3.2.2")]
+[assembly: AssemblyVersion("1.3.3.0")]
+[assembly: AssemblyFileVersion("1.3.3.0")]
